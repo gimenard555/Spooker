12246c727b03fecdc7aec2d36c17f73835dd2192
Jimena Rojas <gimenard555@gmail.com>
2021-12-10 19:09:18 -0500

    10 Dic 2021 Configuracion Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:spooker/ui/screens/login_screen.dart';
 
-void main() => runApp(ProviderScope(child: SpookerApp()));
+final firebaseAuthProvider =
+    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
-class SpookerApp extends StatelessWidget {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(ProviderScope(
+    child: SpookerApp(),
+  ));
+}
+
+class SpookerApp extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Spooker',
+      debugShowCheckedModeBanner: false,
       home: LoginScreen(),
+      /* onGenerateRoute: (settings) =>
+          AppRouter.onGenerateRoute(settings, firebaseAuth),*/
     );
   }
 }
